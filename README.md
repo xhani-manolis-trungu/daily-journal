58694f15bd32c69afe32b107b6ee1f3e57da8fc6
xhani-manolis-trungu <trmanolis@gmail.com>
2018-11-15 02:30:44 +0200

    API fully functional, only angularMaterial UI doesnt appear for now

--- README.md
+++ README.md
@@ -25,3 +25,11 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+## Installing MongoDB
+
+Read from this source: https://docs.mongodb.com/manual/tutorial/install-mongodb-on-debian/
+
+## Mongoose Schemas
+
+Read from this source: https://mongoosejs.com/docs/schematypes.html
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,78 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule, MatCheckboxModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule, MatIconModule, MatFormFieldModule, MatInputModule, MatSortModule, MatTableModule } from '@angular/material';
+import { MatMenuModule} from '@angular/material/menu';
+import { MatCardModule} from '@angular/material/card';
+import { BookComponent } from './book/book.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BookCreateComponent } from './book-create/book-create.component';
+import { BookEditComponent } from './book-edit/book-edit.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ApiService } from './api.service';
+
+const appRoutes: Routes = [
+  {
+    path: 'books',
+    component: BookComponent,
+    data: { title: 'Book List' }
+  },
+  {
+    path: 'book-details/:id',
+    component: BookDetailComponent,
+    data: { title: 'Book Details' }
+  },
+  {
+    path: 'book-create',
+    component: BookCreateComponent,
+    data: { title: 'Create Book' }
+  },
+  {
+    path: 'book-edit/:id',
+    component: BookEditComponent,
+    data: { title: 'Edit Book' }
+  },
+  { path: '',
+    redirectTo: '/books',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent,
+    BookDetailComponent,
+    BookCreateComponent,
+    BookEditComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
-    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     BrowserAnimationsModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatProgressSpinnerModule,
+    MatIconModule,
     MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    AppRoutingModule,
     MatCheckboxModule,
     MatMenuModule,
-    MatCardModule
+    MatProgressBarModule,  
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
